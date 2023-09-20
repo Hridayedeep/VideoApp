9217d53155894050fa49f88d11094b79bfd1c112
Hridayedeep Gupta <hridayedeepgupta@gmail.com>
2023-09-20 23:18:29 +0530

    added table view to show the list items

---
+++ VideoApp/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  VideoApp
+//
+//  Created by Hridayedeep Gupta on 20/09/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    private var titleLabel: UILabel!
+    private var rightIcon: UIImage!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- VideoApp/ViewController.swift
+++ VideoApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView = UITableView()
+        tableView.backgroundColor = .white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: String(describing: TableViewCell.self))
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
 
-
+    private func fetchData() {
+        // fetch from JSON
+        tableView.reloadData()
+    }
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: TableViewCell.self)) else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
+
+
 
